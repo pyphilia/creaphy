ff6364414fe5e4fa62e733b3105115a5b04365cf
KL <kim.phanhoang@epfl.ch>
2017-01-31 22:43:54 +0100

    Add navigation and aboutme php files

---
+++ aboutme.php
@@ -0,0 +1,13 @@
+<section id="aboutme">
+	
+	<p>
+		- y.o: 19 years old<br/>
+		- status: Computer Science Bachelor student<br/>
+		- country: Switzerland<br/>
+		- contact: pyphilia@gmail.com<br/>
+		- HTML 4 	CSS 4		JS 3		PHP/SQL 3<br/>
+		- C 1.5		Java/Scala 2.5<br/>
+		- icons (kpop, pokemon, graphism, nail)<br/>
+	</p>
+	
+</section>
