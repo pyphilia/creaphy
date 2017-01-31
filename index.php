b3d3a6a3f208c729480a21ef8a974aebce56bc08
KL <kim.phanhoang@epfl.ch>
2017-01-31 22:15:27 +0100

    Add bootstrap

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Title of the document</title>
+	<meta charset="UTF-8">
+	<title>Title of the document</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.css">
 </head>
 
 <body>
-Content
+	Content
 
-<script src="js/jquery-3.1.1.min.js"></script>
+	<script src="js/jquery-3.1.1.min.js"></script>
+	<script src="lib/bootstrap/js/bootstrap.js"></script>
 
-<script>
-$( document ).ready(function() {
- 
-    $( "a" ).click(function( event ) {
- 
-        alert( "Thanks for visiting!" );
- 
-    });
- 
-});
-</script>
+	<script>
+		$( document ).ready(function() {
+
+			$( "a" ).click(function( event ) {
+
+				alert( "Thanks for visiting!" );
+
+			});
+
+		});
+	</script>
 
 </body>
 
