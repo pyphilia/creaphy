04273e3f0f3605c8aa339faddfa552c502890301
KL <kim.phanhoang@epfl.ch>
2017-01-31 22:44:41 +0100

    Add some links to index.php

--- index.php
+++ index.php
@@ -2,25 +2,25 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Title of the document</title>
-	<link rel="stylesheet" href="css/styles.css">
+	<title>Créaphy</title>
+	<link rel="stylesheet" href="css/build/css/styles.css">
 	<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.css">
 </head>
 
 <body>
-	Content
-
 	<script src="js/jquery-3.1.1.min.js"></script>
 	<script src="lib/bootstrap/js/bootstrap.js"></script>
 
+
+	<?php include("navigation.php"); ?>
+	<?php include("home.php"); ?>
+	<?php include("aboutme.php"); ?>
+
+	<div class="test">BIG SPACE FOR TEST</div>
+
+
 	<script>
 		$( document ).ready(function() {
-
-			$( "a" ).click(function( event ) {
-
-				alert( "Thanks for visiting!" );
-
-			});
 
 		});
 	</script>
